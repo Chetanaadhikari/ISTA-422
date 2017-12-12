47f692c0f42d9526296388141de93c10b1cfe889
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-12-12 11:18:27 -0500

    Add files via upload

---
+++ Labs/ASP7/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPCLASSWORK1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "GoodMorning" : "GoodAfternoon";
+            return View();
+        }
+    }
+}
